173f70b403ea2eaabd92dbe27ed1694ad6f520e1
rafayet <rafayet@hashkloud.com>
2020-01-19 20:32:01 +0600

    fix select2 bugs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,15 +32,39 @@ import './custom'
 // const imagePath = (name) => images(name, true)
 
 
+$(document).on('turbolinks:load', function() {
+    $('#web-change-locale, #mobile-change-locale').change(function() {
+        this.form.submit();
+    });
+});
+
+$(document).on("turbolinks:click", function(){
+    // show spinner on AJAX start
+    $(".loader").show();
+});
+
+// hide spinner on AJAX stop
+$(document).on("turbolinks:load", function(){
+    $(".loader").hide();
+});
+
+$(document).on('ready page:load', function(event) {
+    $(".loader").hide();
+});
+
 $(document).on("turbolinks:load", function(){
     $(".selection-1").select2({
-        minimumResultsForSearch: 20,
-        dropdownParent: $('#dropDownSelect1')
+        minimumResultsForSearch: 20
     });
 });
 $(document).on("turbolinks:load", function(){
     $(".selection-2").select2({
-        minimumResultsForSearch: 20,
-        dropdownParent: $('#dropDownSelect2')
+        minimumResultsForSearch: 20
+    });
+});
+
+$(document).on("turbolinks:load", function(){
+    $(".mobile-locale-select, .web-locale-select").select2({
+        minimumResultsForSearch: 20
     });
 });
